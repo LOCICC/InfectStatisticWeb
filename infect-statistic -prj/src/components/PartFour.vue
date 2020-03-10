3828923da133bc0b69833c95700f97b65a664af4
RadishBear <1798164846@qq.com>
2020-03-10 21:47:08 +0800

    添加了第一版的前端可执行文件，主要搭起了4个div框架，并加入了一些echarts图标和element-ui组件

---
+++ infect-statistic -prj/src/components/PartFour.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'PartFour'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ infect-statistic -prj/src/components/PartTwo.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'PartTwo'
+  }
+</script>
+
+<style scoped>
+
+</style>
