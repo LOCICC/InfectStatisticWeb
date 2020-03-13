be08adb80994b82ce684a8c38ec3067132f7dfb1
RadishBear <1798164846@qq.com>
2020-03-13 12:56:55 +0800

    加入了第二个数据同步的表格，解决了一个ajax异步的小问题

--- infect-statistic-prj/src/components/LineCharts.vue
+++ infect-statistic-prj/src/components/LineCharts.vue
@@ -6,36 +6,47 @@
   import {init} from '../comm/js/MyEcharts/LineCharts.js'
   import {EventBus} from '../comm/js/tools/bus.js'
   import Vue from 'vue'
+  import axios from 'axios'
   export default {
+    created() {
+      EventBus.$off('ChangeArea') // 解决一次总线请求多次响应的问题
+      EventBus.$off('ChangeLineCharts')
+    },
     mounted () {
       init('LineChart', '新增疑似', data1, '全国')
+      Vue.prototype.$Area = '全国'
       Vue.prototype.$Type = '新增疑似'
       EventBus.$on('ChangeLineCharts', (msg) => {
+        var tempArea = this.$Area
         Vue.prototype.$Type = msg
-        switch (msg) {
-          case '新增疑似':
-            init('LineChart', msg, data1, this.$Area)
-            break
-          case '新增确诊':
-            init('LineChart', msg, data0, this.$Area)
-            break
-          case '治愈':
-            init('LineChart', msg, data2, this.$Area)
-            break
-          case '死亡':
-            init('LineChart', msg, data3, this.$Area)
-            break
-        }
+        axios.get('api/getData_mid.php', {
+          params: {
+            type: msg,
+            province: this.$Area
+          }
+        }).then(function(res) {
+          dataOfLineChart = []
+          var ajaxi = res.data.length
+          var i0 = 0
+          window.console.log(res)
+          while (i0 < ajaxi) {
+            var temp = res.data[i0][msg]
+            dataOfLineChart.push(temp)
+            i0 += 1
+          }
+          init('LineChart', msg, dataOfLineChart, tempArea)
+          window.console.log(dataOfLineChart)
+        }).catch(function (error) {
+          console.log(error)
+        })
       })
       EventBus.$on('ChangeArea', (msg) => {
         init('LineChart', this.$Type, data1, msg)
       })
     }
   }
-  var data0 = [2714, 4586, 7135, 11177, 16678, 22112, 27100, 31728, 33366, 48026, 51986, 54406, 58182]
   var data1 = [1291, 1032, 1347, 2103, 3156, 2447, 2147, 2097, 14840, 2420, 1933, 1639, 631]
-  var data2 = [9, 10, 13, 21, 31, 47, 47, 57, 48, 53, 65, 63, 63]
-  var data3 = [29, 32, 47, 21, 31, 24, 21, 17, 15, 19, 15, 10, 7]
+  var dataOfLineChart = []
 </script>
 
 <style scoped>
