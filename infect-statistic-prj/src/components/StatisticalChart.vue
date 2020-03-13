238be7fbdfd2286f40180f5f1833a06957a7b539
RadishBear <1798164846@qq.com>
2020-03-13 16:36:18 +0800

    加入了对全国的数据初始化,加入了第三张图表的数据沟通

--- infect-statistic-prj/src/components/StatisticalChart.vue
+++ infect-statistic-prj/src/components/StatisticalChart.vue
@@ -11,40 +11,78 @@
       EventBus.$off('ChangeArea') // 解决一次总线请求多次响应的问题
     },
     mounted () {
-      init('StatisticalChart', dataList0, '全国')
+      axios.get('api/getData.php', {
+        params: {
+          province: '全国'
+        }
+      }).then(function(res) {
+        window.console.log(res.data)
+        var ajaxi = res.data.length
+        var i0 = 0
+        dataList0 = []
+        dataOfTime = []
+        dataOfCure = []
+        dataOfDeath = []
+        dataOfIp = []
+        dataOfSp = []
+        while (i0 < ajaxi) {
+          dataOfTime.push(res.data[i0]['日期'])
+          dataOfIp.push(res.data[i0]['现有确诊'])
+          dataOfSp.push(res.data[i0]['现有疑似'])
+          dataOfDeath.push(res.data[i0]['累计死亡'])
+          dataOfCure.push(res.data[i0]['累计治愈'])
+          i0 += 1
+        }
+        dataList0.push(dataOfTime)
+        dataList0.push(dataOfIp)
+        dataList0.push(dataOfSp)
+        dataList0.push(dataOfDeath)
+        dataList0.push(dataOfCure)
+        init('StatisticalChart', dataList0, '全国')
+      }).catch(function (error) {
+        console.log(error)
+      })
       EventBus.$on('ChangeArea', (msg) => {
-        if(msg === '全国') {
-          init('StatisticalChart', dataList0, '全国')
-        } else {
           axios.get('api/getData.php', {
             params: {
               province: msg
             }
           }).then(function(res) {
-            var ajaxi = res.length
+            window.console.log(res.data)
+            var ajaxi = res.data.length
             var i0 = 0
+            dataList0 = []
+            dataOfTime = []
+            dataOfCure = []
+            dataOfDeath = []
+            dataOfIp = []
+            dataOfSp = []
             while (i0 < ajaxi) {
+              dataOfTime.push(res.data[i0]['日期'])
+              dataOfIp.push(res.data[i0]['现有确诊'])
+              dataOfSp.push(res.data[i0]['现有疑似'])
+              dataOfDeath.push(res.data[i0]['累计死亡'])
+              dataOfCure.push(res.data[i0]['累计治愈'])
               i0 += 1
             }
+            dataList0.push(dataOfTime)
+            dataList0.push(dataOfIp)
+            dataList0.push(dataOfSp)
+            dataList0.push(dataOfDeath)
+            dataList0.push(dataOfCure)
             init('StatisticalChart', dataList0, msg)
           }).catch(function (error) {
             console.log(error)
           })
-        }
       })
     }
   }
-  var dataList0 = [
-    ['1.27', '1.29', '1.31', '2.2', '2.4', '2.6', '2.10', '2.12', '2.14', '2.16', '2.18', '2.20', '2.22', '2.24'],
-    [4535, 7440, 11319, 16402, 22980, 29032, 33788, 37693,
-      52599, 56951, 58010, 57886, 55475, 51689, 47760],
-    [6973, 12167, 17988, 21558, 26395, 28942, 21675,
-      16067, 8969, 7264, 5248, 5206, 4148, 2824],
-    [106, 170, 259, 361, 491, 812, 1017, 1368, 1524, 1772,
-      2006, 2239, 2445, 2666],
-    [60, 126, 243, 457, 892, 2651, 3998, 5915, 8101, 9425,
-      14387, 18279, 22907, 27353]
-  ]
+  var dataList0 = []
+  var dataOfTime = []
+  var dataOfIp = []
+  var dataOfSp = []
+  var dataOfDeath = []
+  var dataOfCure = []
 </script>
 
 <style scoped>
